2dbcec3040ea6b3ccb2f82c44d253da66c8c16b2
willclarktech <willclarktech@users.noreply.github.com>
2020-01-07 12:51:22 +0000

    Install: Use Brewfile

--- .dotfiles/install.sh
+++ .dotfiles/install.sh
@@ -2,18 +2,6 @@ $HOME/.vim/install.sh
 
 cat $HOME/.dotfiles/vscode-extensions.txt | xargs -L 1 code --install-extension
 
-brew install \
-	coreutils \
-	emojify \
-	gnupg \
-	goenv \
-	hub \
-	jq \
-	python \
-	python@2 \
-	ruby \
-	shellcheck \
-	tree \
-	wget \
-	yarn \
-	youtube-dl
+cd $HOME/.dotfiles
+brew bundle
+
